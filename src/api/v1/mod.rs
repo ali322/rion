21b4eff24d63603d5f51ee9a9f4c42131d4037dc
chenli13 <chenli13@asiainfo.com>
2023-12-05 17:56:22 +0800

    fix pass

--- src/api/v1/mod.rs
+++ src/api/v1/mod.rs
@@ -1,7 +1,6 @@
 use crate::middleware::JWT;
-use axum::Router;
+use axum::{middleware::from_extractor, Router};
 use std::collections::HashMap;
-use tower_http::auth::RequireAuthorizationLayer;
 
 mod room;
 mod token;
@@ -11,10 +10,9 @@ mod user;
 pub fn apply_routes() -> Router {
     let mut unless = HashMap::new();
     unless.insert(r"^/".to_string(), "get|post|put|delete".to_string());
-    let restrict_layer = RequireAuthorizationLayer::custom(JWT::new(unless));
     token::apply_routes()
         .merge(transceive::apply_routes())
         .merge(user::apply_routes())
         .merge(room::apply_routes())
-        .layer(restrict_layer)
+        .layer(from_extractor::<JWT>())
 }
